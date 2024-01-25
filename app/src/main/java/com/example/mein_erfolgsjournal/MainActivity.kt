6e26424a132b97e59a8b1c8f1671a60ff5d6bae4
dominik_weber <dominikweber.herne@gmail.com>
2024-01-25 18:11:15 +0100

    ganz bisschen code im viewmodel und in der main
    - ALLES OHNE LOGIK NATÜRLICH -

--- app/src/main/java/com/example/mein_erfolgsjournal/MainActivity.kt
+++ app/src/main/java/com/example/mein_erfolgsjournal/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.mein_erfolgsjournal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.example.mein_erfolgsjournal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val navHost =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
     }
 }
--- app/src/main/java/com/example/mein_erfolgsjournal/ui/MainViewModel.kt
+++ app/src/main/java/com/example/mein_erfolgsjournal/ui/MainViewModel.kt
@@ -1,4 +1,11 @@
 package com.example.mein_erfolgsjournal.ui
 
-class MainViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.example.mein_erfolgsjournal.data.Repository
+
+class MainViewModel(application: Application):AndroidViewModel(application) {
+
+    val repository = Repository()
+
 }
